3568d97a42c329a7a7a54ad53e711d0379b9eb91
unknown <chetanzagade28@gmail.com>
2024-10-29 23:53:14 +0530

    Remove unwanted folders

--- part1/src/Content.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Content() {
-  return <div>Hi</div>;
-}
-
-export default Content;
--- part1/src/Header.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Header(props) {
-  return <div>{props.header}</div>;
-}
-
-export default Header;
